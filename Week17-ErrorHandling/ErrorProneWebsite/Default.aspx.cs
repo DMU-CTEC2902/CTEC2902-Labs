fc41080a920af3d0ddcb4398efd13224197d0189
DaveGerrardDMU <dave.gerrard@dmu.ac.uk>
2015-12-23 14:34:23 +0000

    Added the solution file from Untracked Files

--- Week17-ErrorHandling/ErrorProneWebsite/Default.aspx.cs
+++ Week17-ErrorHandling/ErrorProneWebsite/Default.aspx.cs
@@ -13,11 +13,17 @@ namespace ErrorProneWebsite
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // The first two lines find and set the name of the directory and file that the content is stored in
+            
             string appDataDirectory = Server.MapPath("/App_Data");
 
             string contentFilePath = string.Format(@"{0}\{1}", appDataDirectory, "Content.txt");
 
+            // Then a file manager is created to read the content from the file
+
             FileManager contentManager = new FileManager(contentFilePath);
+
+            // The text property of the label contained in the ASPX file is set with the content returned by the FileManager 
 
             lblContent.Text = contentManager.GetContent();
         }
