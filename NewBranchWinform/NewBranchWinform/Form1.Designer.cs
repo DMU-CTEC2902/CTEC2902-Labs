3dce064c71aa277018045bed604b20808859c197
ShakeyDave <david.gerrard@trovo.co.uk>
2016-01-25 15:47:07 +0000

    Added don't click me button

--- NewBranchWinform/NewBranchWinform/Form1.Designer.cs
+++ NewBranchWinform/NewBranchWinform/Form1.Designer.cs
@@ -28,13 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Don\'t click me!";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "The form has a name now";
             this.ResumeLayout(false);
@@ -42,6 +53,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
