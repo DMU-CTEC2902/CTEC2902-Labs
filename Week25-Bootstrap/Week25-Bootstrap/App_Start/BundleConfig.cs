64870457474bb3aeec433daa38ea428d9a6326af
ShakeyDave <david.gerrard@trovo.co.uk>
2016-03-10 17:28:25 +0000

    Updated bootstrap and rendered panels and tables

--- Week25-Bootstrap/Week25-Bootstrap/App_Start/BundleConfig.cs
+++ Week25-Bootstrap/Week25-Bootstrap/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Week25_Bootstrap
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
