b6ea0051ba266e8f08f135413e71855cdf1aa0bd
DaveGerrardDMU <dave.gerrard@dmu.ac.uk>
2016-03-25 17:50:28 +0000

    Set up a Unit Testing project, connected to DB and added first wrapper test.

---
+++ Week26-CleanCodeRefactoring/Shop.Tests.Unit/OrdersControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Web.Mvc; 
+using Week26_CleanCodeRefactoring.Controllers;
+using Week26_CleanCodeRefactoring.Models;
+
+namespace Shop.Tests.Unit
+{
+    [TestClass]
+    public class OrdersControllerTests
+    {
+        [TestMethod]
+        public void TestThatAnOrderIsTakenSuccessfully()
+        {
+            
+            Order order = new Order { 
+                OrderId = 1,
+                CustomerId = 1,
+                DateCreated = new DateTime(2016, 03, 11),
+                PaymentCardId = 1
+            };
+
+            OrdersController ordersController = new OrdersController();
+
+            RedirectToRouteResult result = ordersController.Edit(order) as RedirectToRouteResult;
+
+            Assert.AreEqual("success", result.RouteValues["outcome"]);
+
+        }
+    }
+}
