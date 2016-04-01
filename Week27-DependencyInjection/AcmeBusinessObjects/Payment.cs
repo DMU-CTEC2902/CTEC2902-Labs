52b1f577a2485c672c3c96d0c08c3f5478425d14
ShakeyDave <david.gerrard@trovo.co.uk>
2016-04-01 10:08:31 +0100

    Added ACME Business Objects to illustrate loose coupling benefit

---
+++ Week27-DependencyInjection/AcmeBusinessObjects/Payment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeBusinessObjects
+{
+    public class Payment
+    {
+        public decimal PaymentAmount { get; set; }
+        public string PayerName { get; set; }
+        public string CardNumber { get; set; }
+        public string ExpiryDate { get; set; }
+        public string CVV { get; set; }
+
+    }
+}
---
+++ Week27-DependencyInjection/AcmeBusinessObjects/PaymentProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcmeBusinessObjects
+{
+    public class PaymentProvider
+    {
+
+        public string ResponseCode { get; set; }
+
+        public bool Authorise(Payment payment)
+        {
+            if (payment.CardNumber.Equals("1111222233334444"))
+            {
+                ResponseCode = "AUTHCODE: 57GHL79";
+                return true;
+            }
+
+            ResponseCode = "PAYMENT FAILURE";
+
+            return false;
+        }
+
+    }
+}
