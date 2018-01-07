c1cae4cd155cda6a5b10670d2cd3ad97edb17b88
Sulur <Sulur@SULUR-LAPTOP>
2018-01-07 22:42:29 +0000

    Commite

--- LectureDemoForm/LectureDemoForm/NewForm.cs
+++ LectureDemoForm/LectureDemoForm/NewForm.cs
@@ -10,13 +10,11 @@ using System.Windows.Forms;
 
 namespace LectureDemoForm
 {
-    public partial class Form1 : Form
+    public partial class NewForm : Form
     {
-        public Form1()
+        public NewForm()
         {
             InitializeComponent();
-
-            //more stuff
         }
     }
 }
