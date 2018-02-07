2c7b6deeb7613a711e07b8f17610fb09b04d89ef
maxjmorris <p15213515@my365.dmu.ac.uk>
2018-02-07 10:46:31 +0000

    Added a Console Application Project

---
+++ Week18-BuildingConfiguringAndDeploying/Max Console Application/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Console = Colorful.Console;
+
+namespace Max_Console_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Debug.WriteLine("Write something to the output window...");
+            Console.WriteLine("Program has finished. Press any key to quit.",Color.ForestGreen);
+            Console.ReadLine();
+            Console.WriteLine("Write this!");
+            string greeting = Properties.Settings.Default.GreetingMessage;
+            Console.WriteLine(greeting);
+
+        }
+    }
+}
