272e3a881f25f6849fd6da5bde1e211921b4b8fa
Max Morris <35525228+maxjmorris@users.noreply.github.com>
2018-03-07 09:18:00 +0000

    Add files via upload

---
+++ MinimalMVCProject/Models/NewsStoryModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MinimalMVCProject.Models
+{
+    public class NewsStoryModel
+    {
+
+        public string Headline { get; set; }
+        public string ByLine { get; set; }
+        public string Content { get; set; }
+    }
+}
