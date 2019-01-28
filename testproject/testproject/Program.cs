0c589d7175aa280f085625689fae7c6dcdc6bd9a
shassh <shassh@users.noreply.github.com>
2019-01-28 15:26:35 +0000

    new

--- testproject/testproject/Program.cs
+++ testproject/testproject/Program.cs
@@ -15,6 +15,9 @@ namespace testproject
             Console.WriteLine("Hello World!");
             Console.ReadKey();
 
+
+            //test
+
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
